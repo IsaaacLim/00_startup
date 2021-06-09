a686cb434a3c32582c439dfa3b9ed86c02b77d64
isaaclim <isaaclimjj@yahoo.com>
2021-06-09 17:48:42 +0800

    modified bashprofile.sh

--- bashprofile.sh
+++ bashprofile.sh
@@ -6,20 +6,44 @@ echo "source ~/.bash_prompt" >> ~/.bash_profile #For maintainability
 echo '#!/usr/bin/env bash
 
 # TERMINAL PROMPT
-PS1="\[\e[0;93m\]\u\[\e[m\]"    # username
-PS1+=" "    # space
-PS1+="\[\e[0;95m\](\h)\[\e[m\]"    # host
-PS1+=" "    # space
-PS1+="\[\e[0;92m\]\W\[\e[m\]"    # current directoy
-PS1+=" "    # space
-PS1+=">> "    # end prompt
+# Get current branch in git repo
+function parse_git_branch() {
+	BRANCH=`git branch 2> /dev/null | sed -e "'"/^[^*]/d"'" -e "'"s/* \(.*\)/'\1'/"'"`
+	if [ ! "${BRANCH}" == "" ]
+	then
+		echo "${BRANCH}"
+	else
+		echo ""
+	fi
+}
 
+# Teminal text
+PS1="\\033[01;38;5;210misaac\\033[0m"    # username
+PS1+="\\033[02;03;38;5;218m\](\h)\\033[0m"    # host
+PS1+=" "    # space
+PS1+="\\033[0;38;5;153m\]\W\\033[0m"    # current directoy
+PS1+=" "    # space
+PS1+="\\033[38;5;228m\$(parse_git_branch)\\033[0m"
+PS1+=" "    # space
+PS1+="\\033[38;5;195m\]>> "    # end prompt
+
+# Teminal text setting
 export PS1;
 export CLICOLOR=1 #Turns color on
 export LSCOLORS=ExFxBxDxCxegedabagacad #Customizes them
 
+# NOTES
+# Other options: \u (username)
+# From https://medium.com/@charlesdobson/how-to-customize-your-macos-terminal-7cce5823006e
 
-#https://medium.com/@charlesdobson/how-to-customize-your-macos-terminal-7cce5823006e
+# Color setting
+# "\\033[48;5;__;38;5;__mTEXT\\033[0m" 
+# 48;5 for background
+# 38;5 for foreground
+# fill 3 digit color num from reference below in __ spaces
+# Add 01 before for bold
+# From https://unix.stackexchange.com/questions/124407/what-color-codes-can-i-use-in-my-ps1-prompt
+
 ' > ~/.bash_prompt 
 
 chmod 744 ~/.bash_prompt
