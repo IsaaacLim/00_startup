bcb40f9171754fc0d528fdef4ea149496fbf409d
isaaclim <isaaclimjj@yahoo.com>
2021-06-09 18:42:59 +0800

    zsh_prompt

--- zsh_prompt.sh
+++ zsh_prompt.sh
@@ -4,10 +4,20 @@ echo "#Command Prompt Styling
 TERM=xterm-256color
 PROMPT='%U%F{217}isaac%f%F{139}(%m)%f%u %U%F{152}%1~%f%u %F{252}➜%f '" > ~/.zshrc
 
+echo "
+#Git Branch to Right of Command Prompt
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+setopt prompt_subst
+RPROMPT=\$vcs_info_msg_0_
+zstyle ':vcs_info:git:*' formats '%F{240}(%b)%r%f'
+zstyle ':vcs_info:*' enable git " >> ~/.zshrc
+
 # From
 #https://blog.devgenius.io/customize-the-macos-terminal-zsh-4cb387e4f447
 
-# Another good resource
+# Another good resource for additional formatting
 #https://scriptingosx.com/2019/07/moving-to-zsh-06-customizing-the-zsh-prompt/
 
 # TERMINAL COLOR
