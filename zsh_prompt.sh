2459a53ac8be33fac3e1771898f9214f443de6c1
isaaclim <isaaclimjj@yahoo.com>
2021-06-09 18:58:27 +0800

    zsh_prompt

--- zsh_prompt.sh
+++ zsh_prompt.sh
@@ -10,8 +10,8 @@ autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-RPROMPT=\$vcs_info_msg_0_
-zstyle ':vcs_info:git:*' formats '%F{240}(%b)%r%f'
+RPROMPT=\\$vcs_info_msg_0_
+zstyle ':vcs_info:git:*' formats '%F{240}(%b)branch%f'
 zstyle ':vcs_info:*' enable git " >> ~/.zshrc
 
 # From
