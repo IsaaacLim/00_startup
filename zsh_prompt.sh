789255f3b15b506f34fd82b6be3076ede3aefa78
isaaclim <isaaclimjj@yahoo.com>
2021-06-09 18:32:13 +0800

    zsh_prompt

--- zsh_prompt.sh
+++ zsh_prompt.sh
@@ -2,7 +2,7 @@
 
 echo "#Command Prompt Styling
 TERM=xterm-256color
-PROMPT='%S%F{217}isaac%f%s %B%F{139}(%m)%f%b %U%F{152}%1~%f%u %F{252}➜%f '" > ~/.zshrc
+PROMPT='%B%F{217}isaac%f%b %B%F{139}(%m)%f%b %U%F{152}%1~%f%u %F{252}➜%f '" > ~/.zshrc
 
 # From
 #https://blog.devgenius.io/customize-the-macos-terminal-zsh-4cb387e4f447
