eeafe9693bff7cf620f44916a7683ef2b4649291
Jinq jia Lim <jinlim@u90z04s03.42kl.edu.my>
2021-06-12 13:06:33 +0800

    fixed alias script

--- alias.sh
+++ alias.sh
@@ -3,3 +3,6 @@ alias gccw="gcc -Wall -Wextra -Werror *.c && ./a.out"
 alias gcch="gcc -Wall -Wextra -Werror *.c *.h && ./a.out"
 alias normch="norminette -R CheckForbiddenSourceHeader"
 alias normi="~/.local/bin/norminette"
+
+alias gccA="gcc main.c libftprintf.a && ./a.out | cat -e"
+alias gccO="gcc -Wall -Wextra -Werror output.c && ./a.out | cat -e"
